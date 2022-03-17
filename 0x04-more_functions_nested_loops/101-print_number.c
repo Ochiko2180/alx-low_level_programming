d2ce54b6eb6b9ace8622099c84e976c87170fcf4
Angela Alwenje <ochikoalwenje@gmail.com>
2022-03-17 13:04:10 -0700

    My commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,31 @@
 */
 void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int num, num2;
+	int i;
+	int aux = 1;
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
+		n = n * -1;
 		_putchar('-');
 	}
-
-	k /= 10;
-
-	if (k != 0;
-	print_number(k);
-
-	_putchar((unsigned int) n % 10 + '0');
+	num = n;
+	num2 = num;
+	if (num > 9)
+	{
+		while (num >= 10)
+		{
+			aux = aux * 10;
+			num = num / 10;
+		}
+		_putchar((num2 / aux) + '0');
+		aux = aux / 10;
+		
+		for (i = aux; i >= 1; i = i / 10)
+			_putchar((num2 / i) % 10 + '0');
+	}
+	else
+		_putchar(num + '0');
 
 }
